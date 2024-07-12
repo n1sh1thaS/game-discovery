3c5bd2bf65b91cdd8cfa4c086e678a2b3b0a12cb
nselvakumar25 <nselvakumar@ucsd.edu>
2024-07-11 19:22:42 -0700

    create color mode feature with toggle

--- src/components/NavBar.tsx
+++ src/components/NavBar.tsx
@@ -1,12 +1,13 @@
 import React from "react";
 import { HStack, Image, Text } from "@chakra-ui/react";
 import logo from "../assets/logo.webp";
+import ColorMode from "./ColorMode";
 
 const NavBar = () => {
   return (
-    <HStack>
+    <HStack justifyContent={"space-between"} margin-right={"10px"}>
       <Image src={logo} boxSize={"60px"}></Image>
-      <Text>NavBar</Text>
+      <ColorMode />
     </HStack>
   );
 };
